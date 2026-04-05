52c63000502cc34d5afb1f6abf8438d25ccdf5e5
90vamshikarthik-star <90vamshikarthik@gmail.com>
2026-04-06 00:29:37 +0530

    final fix openenv validation

--- server/app.py
+++ server/app.py
@@ -1,6 +1,12 @@
-# server/app.py
-
 from fastapi import FastAPI
+import uvicorn
 from app.main import app as main_app
 
 app = main_app
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
